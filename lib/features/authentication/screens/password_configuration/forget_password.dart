aa42d10f2737a19dbf1e3f1c3c24b6cf63cdbbba
deepankarvarma <satwikdpshrit@gmail.com>
2023-12-01 18:11:04 +0530

    11

---
+++ lib/features/authentication/screens/password_configuration/forget_password.dart
@@ -0,0 +1,16 @@
+import 'package:college_saathi/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Padding(
+        padding: EdgeInsets.all(TSizes.defaultSpace),
+        ),
+    );
+  }
+}

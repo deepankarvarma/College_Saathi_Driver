631c39e948ef079a9313d05b58a790ed687b9320
adityasrathore <adityasr2000@gmail.com>
2023-12-01 19:05:05 +0530

    verify email

--- lib/features/authentication/screens/signup/verify_email.dart
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -23,7 +23,7 @@ class VerifyEmailScreen extends StatelessWidget{
                     child: Column(
                         children: [
                             Image(
-                              image: AssetImage(TImages.deliveredEmailIllustration),
+                              image: const AssetImage(TImages.deliveredEmailIllustration),
                               width: THelperFunctions.screenWidth()*0.6,
                             ),
                             const SizedBox(height: TSizes.spaceBtwSections,),
